517ed27dac6ca8e8660275c6f13edb2b75530cd5
denis <permwed@gmail.com>
2022-09-13 23:08:00 +0500

    start header and do scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Photostore!</h1>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div>
+            <h3 className="headerInfo">React photostore</h3>
+            <p>Магазин лучших фотоаппаратов</p>
+          </div>
+        </div>
+        <div className="headerRight">
+          <ul>
+            <li>
+              <svg />
+              <span>1205 руб.</span>
+            </li>
+            <li>
+              <svg />
+            </li>
+          </ul>
+        </div>
+      </header>
+      <div className="content">
+        <h1>Все фотоаппараты</h1>
+        ...
+      </div>
     </div>
   );
 }
