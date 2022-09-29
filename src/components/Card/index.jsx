751dfec8fce9b28b03d4e1886875cb5d2ed49d72
denis <permwed@gmail.com>
2022-09-29 14:28:55 +0500

    add react skeleton

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import ContentLoader from "react-content-loader";
+
 // Css
 import styles from "./Card.module.scss";
 
@@ -27,7 +29,20 @@ function Card({
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite}>
+      <ContentLoader
+        speed={2}
+        width={220}
+        height={260}
+        viewBox="0 0 220 260"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="0" y="160" rx="10" ry="10" width="170" height="28" />
+        <rect x="0" y="220" rx="10" ry="10" width="110" height="38" />
+        <rect x="130" y="220" rx="10" ry="10" width="40" height="40" />
+        <rect x="0" y="0" rx="10" ry="10" width="170" height="150" />
+      </ContentLoader>
+      {/* <div className={styles.favorite}>
         <img
           onClick={handleFavorite}
           width={31}
@@ -52,7 +67,7 @@ function Card({
             alt="Добавить товар"
           />
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
